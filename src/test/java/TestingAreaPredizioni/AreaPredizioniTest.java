87f086e4cb9964bf67edee4cfea29f932713d1a4
MGiella <m.giella4@studenti.unisa.it>
2023-02-08 12:09:01 +0100

    Aggiunti test per il package MLmodel

--- src/test/java/TestingAreaPredizioni/AreaPredizioniTest.java
+++ src/test/java/TestingAreaPredizioni/AreaPredizioniTest.java
@@ -3,16 +3,26 @@ package TestingAreaPredizioni;
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.response.Response;
+import org.acme.Device;
 import org.acme.GestioneAreaPredizioni.MachineLearning.MLModel;
+import org.acme.GestioneAreaPredizioni.MachineLearning.PredizioniAteroService;
+import org.acme.GestioneAreaPredizioni.MachineLearning.PredizioniInfartoService;
 import org.acme.GestioneAreaPredizioni.PredizioniResources;
 import org.acme.GestioneAreaPredizioni.PublisherSubscriber.PredictionGenerator;
 import org.acme.GestioneAreaPredizioni.PublisherSubscriber.Predizione;
 import org.hamcrest.Matcher;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import weka.classifiers.Evaluation;
+import weka.classifiers.functions.LinearRegression;
+import weka.core.converters.ConverterUtils;
+
 import javax.inject.Inject;
 import java.util.List;
+import java.util.Random;
+
 import static io.restassured.RestAssured.given;
+import static io.smallrye.common.constraint.Assert.assertTrue;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -21,6 +31,8 @@ public class AreaPredizioniTest {
 
     @Inject
     PredictionGenerator p;
+    @Inject
+    MLModel ml;
 
     @Test
     @Order(4)
@@ -104,6 +116,61 @@ public class AreaPredizioniTest {
     }
 
 
+    @Test
+    @Order(5)
+    public void CheckProbabiltyAtero() throws Exception {
+        //calcola la predizione effettiva
+        List<Device> rilevazioni = this.p.getRilevazioni();
+        double predizioneEffettiva = 0;
+        for(Device ril : rilevazioni){
+            predizioneEffettiva += ml.calcolaPrediction(ril,"atero");
+        }
+        predizioneEffettiva = predizioneEffettiva/rilevazioni.size();
+
+        //fa la predizione tramite modello e lo ottiene
+        double predizioniCalcolate = PredizioniAteroService.getPredizioneAtero(rilevazioni).getPercentualeRischio();
+        ConverterUtils.DataSource source = new ConverterUtils.DataSource(ml.getArff("atero",rilevazioni));
+        LinearRegression model = ml.getModel(source);
+
+        //ottengo il mean absolute error per verificare lo scarto entro il quale deve essere preciso
+        Evaluation eval= new Evaluation(PredizioniAteroService.getAsInstanceAtero(rilevazioni));
+        eval.crossValidateModel(model,PredizioniAteroService.getAsInstanceAtero(rilevazioni),10,new Random());
+        double meanAbsoluteError = eval.meanAbsoluteError();
+
+        System.out.println(predizioniCalcolate + "%\n" + predizioneEffettiva +"%\nMAE\t " + meanAbsoluteError);
+        //verifica se la predizione calcolata è uguale alla predizione effettiva per uno scarto di MAE
+        assertTrue(predizioniCalcolate <= predizioneEffettiva + meanAbsoluteError &&
+                predizioniCalcolate >= predizioneEffettiva - meanAbsoluteError   );
+
+    }
+
+    @Test
+    @Order(6)
+    public void CheckProbabiltyInfarto() throws Exception {
+        //calcola la predizione effettiva
+        List<Device> rilevazioni = this.p.getRilevazioni();
+        double predizioneEffettiva = 0;
+        for(Device ril : rilevazioni){
+            predizioneEffettiva += ml.calcolaPrediction(ril,"infarto");
+        }
+        predizioneEffettiva = predizioneEffettiva/rilevazioni.size();
+
+        //fa la predizione tramite modello e lo ottiene
+        double predizioniCalcolate = PredizioniInfartoService.getPredizioneInfarto(rilevazioni).getPercentualeRischio();
+        ConverterUtils.DataSource source = new ConverterUtils.DataSource(ml.getArff("infarto",rilevazioni));
+        LinearRegression model = ml.getModel(source);
+
+        //ottengo il mean absolute error per verificare lo scarto entro il quale deve essere preciso
+        Evaluation eval= new Evaluation(PredizioniInfartoService.getAsInstanceInfarto(rilevazioni));
+        eval.crossValidateModel(model,PredizioniInfartoService.getAsInstanceInfarto(rilevazioni),10,new Random());
+        double meanAbsoluteError = eval.meanAbsoluteError();
+
+        System.out.println(predizioniCalcolate + "%\n" + predizioneEffettiva +"%\nMAE\t " + meanAbsoluteError);
+        //verifica se la predizione calcolata è uguale alla predizione effettiva per uno scarto di MAE
+        assertTrue(predizioniCalcolate <= predizioneEffettiva + meanAbsoluteError &&
+                predizioniCalcolate >= predizioneEffettiva - meanAbsoluteError   );
+
+    }
 
 
     //restituisce true se il rischio corrisponde alla percentuale corrispondente
